38438780f2f16f7927ccc1de276160dc3c859002
jacobdlewis <jacobdlewis@gmail.com>
2015-04-24 11:51:32 -0500

    cal.rb requires day class

--- cal.rb
+++ cal.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
 require_relative 'lib/month'
+require_relative 'lib/day'
 
 month = ARGV[0]
 year = ARGV[1]
 
 Default_error_msg = "Date not in acceptable format/range."
 
-month = Month.new(month, year)
+m = Month.new(month, year)
 puts m.to_s
 
