59d4c03c6aecb62e24a6359b85e8f6770860366f
jacobdlewis <jacobdlewis@gmail.com>
2015-04-24 12:05:56 -0500

    cal.rb outputs individual months. all unit and integration tests passing:

--- cal.rb
+++ cal.rb
@@ -3,11 +3,17 @@
 require_relative 'lib/month'
 require_relative 'lib/day'
 
-month = ARGV[0]
-year = ARGV[1]
+month = ARGV[0].to_i
+year = ARGV[1].to_i
 
 Default_error_msg = "Date not in acceptable format/range."
 
-m = Month.new(month, year)
-puts m.to_s
+if month == 0 || month > 12
+  print Default_error_msg
+elsif year < 1800 || year > 3000
+  print Default_error_msg
+else
+  m = Month.new(month, year)
+  puts m.to_s
+end
 
