309853330b840aeceb60aeb3b65bef749d8cc7c0
jacobdlewis <jacobdlewis@gmail.com>
2015-04-26 23:53:57 -0500

    year to_s method passes all tests

--- lib/year.rb
+++ lib/year.rb
@@ -10,7 +10,7 @@ class Year
     year = @year
     months = []
     month_names = []
-    year_header = "#{year}".center(63) + "\n" + "\n"
+    year_header = "#{year}".rjust(33) + "\n" + "\n"
 
     (1..12).each do |x|
       m = Month.new(x, year)
@@ -25,21 +25,22 @@ class Year
     str_printout = "#{year_header}"
 
     months.each_slice(3) do |row|
+    month_row = "#{month_names.shift.center(20)}  #{month_names.shift.center(20)}  #{month_names.shift.center(20)}"
+
       row[0][6] = "                    " if row[0][6].nil?
       row[1][6] = "                    " if row[1][6].nil?
-      row[2][6] = "                    " if row[2][6].nil?
       row[0][5] = "                    " if row[0][5].nil?
       row[1][5] = "                    " if row[1][5].nil?
       row[2][5] = "                    " if row[2][5].nil?
       str_printout << <<EOS
-#{month_names.shift.center(20)}  #{month_names.shift.center(20)}  #{month_names.shift.center(20)}
+#{month_row.rstrip}
 #{row[0][0]}  #{row[1][0]}  #{row[2][0]}
 #{row[0][1]}  #{row[1][1]}  #{row[2][1]}
 #{row[0][2]}  #{row[1][2]}  #{row[2][2]}
 #{row[0][3]}  #{row[1][3]}  #{row[2][3]}
 #{row[0][4]}  #{row[1][4]}  #{row[2][4]}
-#{row[0][5].ljust(20)}  #{row[1][5].ljust(20)}  #{row[2][5].ljust(20)}
-#{row[0][6]}  #{row[1][6]}  #{row[2][6]}
+#{row[0][5].ljust(20)}  #{row[1][5].ljust(20)}  #{row[2][5].ljust(20).rstrip}
+#{row[0][6].ljust(20)}  #{row[1][6].ljust(20)}  #{row[2][6]}
 EOS
     end
   str_printout
