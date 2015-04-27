6de77bdcaa447749349994123679e479a5eabc88
jacobdlewis <jacobdlewis@gmail.com>
2015-04-26 23:37:23 -0500

    program accepts one or two arguments

--- cal.rb
+++ cal.rb
@@ -2,18 +2,30 @@
 
 require_relative 'lib/month'
 require_relative 'lib/day'
-
-month = ARGV[0].to_i
-year = ARGV[1].to_i
+require_relative 'lib/year'
 
 Default_error_msg = "Date not in acceptable format/range."
 
-if month == 0 || month > 12
-  print Default_error_msg
-elsif year < 1800 || year > 3000
-  print Default_error_msg
-else
-  m = Month.new(month, year)
-  puts m.to_s
-end
+if ARGV.length == 1
+  year = ARGV[0].to_i
 
+  if year < 1800 || year > 3000
+    print Default_error_msg
+  else
+    y = Year.new(year)
+    puts y.to_s
+  end
+elsif ARGV.length == 2
+  month = ARGV[0].to_i
+  year = ARGV[1].to_i
+
+  if month == 0 || month > 12
+    print Default_error_msg
+  elsif year < 1800 || year > 3000
+    print Default_error_msg
+  else
+    m = Month.new(month, year)
+    puts m.to_s
+  end
+
+end
